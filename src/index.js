c3ebcf6750427a767be9ce35f6c00b49c536f301
Roie Cohen <roie.cohen@gmail.com>
2018-12-12 14:27:38 -0800

    moved heroku

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const resolvers = require('./resolvers')
 
 const db = new Prisma({
   typeDefs: 'src/generated/prisma.graphql', // the auto-generated GraphQL schema of the Prisma API
-  endpoint: 'https://maana-incubator.herokuapp.com/nlp', // the endpoint of the Prisma API (value set in `.env`)
+  endpoint: 'https://maana-r-incubator-481a59bd4b.herokuapp.com/nlp', // the endpoint of the Prisma API (value set in `.env`)
   debug: true // log all GraphQL queries & mutations sent to the Prisma API
   // secret: process.env.PRISMA_SECRET, // only needed if specified in `database/prisma.yml` (value set in `.env`)
 })
