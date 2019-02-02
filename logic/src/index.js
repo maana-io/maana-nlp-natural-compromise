f0039092fd9d8241b6a509984315308150df89bb
Roie Cohen <roie.cohen@gmail.com>
2019-02-02 14:15:20 -0800

    restructure

--- logic/src/index.js
+++ logic/src/index.js
@@ -18,4 +18,4 @@ const server = new GraphQLServer({
   context: req => ({ ...req, db })
 })
 
-server.start(() => console.log('Server is running on localhost:4000'))
+server.start(() => console.log('Server is running on http://localhost:4000'))
